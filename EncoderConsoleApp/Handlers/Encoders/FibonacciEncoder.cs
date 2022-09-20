f20ffd285957dcfc62a6606013c0e541c5e10df5
Sandro <sandro_junior@hotmail.com>
2022-09-20 18:35:13 -0300

    fibonacci encoder

--- EncoderConsoleApp/Handlers/Encoders/FibonacciEncoder.cs
+++ EncoderConsoleApp/Handlers/Encoders/FibonacciEncoder.cs
@@ -1,5 +1,7 @@
-﻿using EncoderConsoleApp.Handlers.Encoders.Interfaces;
+﻿using EncoderConsoleApp.Enums;
+using EncoderConsoleApp.Handlers.Encoders.Interfaces;
 using EncoderConsoleApp.Handlers.Files;
+using EncoderConsoleApp.Utils;
 
 namespace EncoderConsoleApp.Handlers.Encoders;
 
@@ -14,7 +16,49 @@ public class FibonacciEncoder : IFibonacciEncoder
 
     public void Encode(byte[] bytes, int divisor)
     {
-        throw new NotImplementedException();
+        var fibonacci = GenerateFibonacciSequence();
+        var bytesWithSum = BytesUtils.AddOneInEachByte(bytes);
+        
+        var strList = new List<string>();
+        BuildHeader(strList, 0);
+        
+        var fullStr = "";
+
+        foreach (var t in bytesWithSum)
+        {
+            var fibonacciFilter = fibonacci.Where(x => x <= t).ToArray();
+            var str = "1";
+            var sum = 0;
+            
+            foreach (var item in fibonacciFilter)
+            {
+                if (sum + item <= t)
+                {
+                    str += "1";
+                    sum += item;
+                    
+                    continue;
+                }
+                
+                str += "0";
+            }
+            
+            var strReverse = new string(str.Reverse().ToArray());
+            fullStr += strReverse;
+        }
+
+        var fullList = StringUtils
+            .Split8BitsList(strList)
+            .Select(x => Convert.ToByte(x, 2)).ToList();
+            
+        var list = StringUtils
+            .Split8BitsList(fullStr)
+            .Select(x => Convert.ToByte(x, 2))
+            .ToList();
+        
+        fullList.AddRange(list);
+        
+        _fileHandler.Write(fullList.ToArray(), OperationType.Encode);
     }
 
     public void Decode(byte[] bytes, int divisor)
@@ -29,5 +73,17 @@ public class FibonacciEncoder : IFibonacciEncoder
         
         list.Add(firstByte);
         list.Add(secondByte);
+    }
+
+    private int[] GenerateFibonacciSequence()
+    {
+        var fibonacciArray = new int[12];
+        fibonacciArray[0] = 1;
+        fibonacciArray[1] = 2;
+        
+        for (int i = 2; i < fibonacciArray.Length; i++) {
+            fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+        }
+        return fibonacciArray.Reverse().ToArray();
     }
 }
--- EncoderConsoleApp/Utils/StringUtils.cs
+++ EncoderConsoleApp/Utils/StringUtils.cs
@@ -19,4 +19,21 @@ public class StringUtils
 
         return newList;
     }
+    
+    public static IList<string> Split8BitsList(string str)
+    {
+        var newList = new  List<string>();
+        var strControl = str;
+        
+        while (strControl.Length >= 8)
+        {
+            newList.Add(strControl[..8]);
+            strControl = strControl[8..];
+        }
+
+        var fillWithZero = strControl.PadRight(8, '0');
+        newList.Add(fillWithZero);
+
+        return newList;
+    }
 }
